c12a3394cfb0b287cdb841971b672e1ad7c5e406
Vivek Raman <VR.AC4BF@live.com>
2019-04-21 13:20:16 +0530

    Added files to repository
    Currently working on Scoring System, exploit fixes, and polish

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float TimeBetweenSpawns = 3f;
+
+    [Header("Debug")]
+    [SerializeField] private List<Image> MoleImageComponents = new List<Image>();
+    [SerializeField] private Score ScoreReference;
+
+    bool isMoleSmashed = false;
+    int selectedIndex = 0;
+
+    void Start()
+    {
+        // spawn moles
+        StartCoroutine(MoleSpawner());
+
+    }
+
+    IEnumerator MoleSpawner()
+    {
+        while (true)
+        {
+            Spawn();
+            yield return new WaitForSeconds(TimeBetweenSpawns);
+        }
+    }
+
+    private void Spawn()
+    {
+        isMoleSmashed = false;
+        selectedIndex = Random.Range(1, 9);
+        MoleImageComponents[selectedIndex].enabled = true;
+    }
+
+    // DO NOT RENAME THIS, you'll have to re-map the buttons through inspector
+    public void Hole(int holeIndex)
+    {
+        MoleImageComponents[selectedIndex].enabled = false;
+        if (holeIndex == selectedIndex + 1)
+        {
+            if (!isMoleSmashed)
+            {
+                ScoreReference.AddScore(1f);
+            }
+            isMoleSmashed = true;
+        }
+        else
+        {
+            ScoreReference.AddScore(-1f);
+        }
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [Header("Debug")]
+    [SerializeField] private Text scoreTextReference;
+    [SerializeField] private GameManager gameManager;
+    [SerializeField] private float counter = 0f;
+    
+    public void AddScore(float scoreToAdd = 1f)
+    {
+        counter += scoreToAdd;
+        scoreTextReference.text = counter.ToString();
+    }
+}
